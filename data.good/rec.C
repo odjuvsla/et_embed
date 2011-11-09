d2dd272e90a290c5f96c23e7d6b10e447df23b0f
Oystein Djuvsland <oystein.djuvsland@gmail.com>
2011-11-09 22:20:11 +0100

    - adding example data

---
+++ data.good/rec.C
@@ -0,0 +1,40 @@
+void rec(TString filename = "raw.root") {
+  AliReconstruction reco;
+
+  //  reco.SetWriteESDfriend();
+  //  reco.SetWriteAlignmentData();
+  AliCDBManager * man = AliCDBManager::Instance() ;
+  //   man->SetDefaultStorage("local://$ALICE_ROOT/OCDB") ;
+   man->SetDefaultStorage("raw://") ;
+      //  man->SetRun(118339) ;
+  man->SetRun(137366) ;
+
+  //  reco.SetDefaultStorage("local://$ALICE_ROOT/OCDB");
+  //  reco.SetSpecificStorage("GRP/GRP/Data",
+  //  			  Form("local://%s",gSystem->pwd()));
+  reco.SetRunPlaneEff(kFALSE);
+
+  //  reco.SetFractionFriends(1.);
+
+
+   reco.SetRunLocalReconstruction(":");
+  //  reco.SetRunReconstruction("PHOS EMCAL");
+   reco.SetRunLocalReconstruction("PHOS");
+
+  reco.SetRunHLTTracking(kFALSE);
+  reco.SetRunVertexFinder(kFALSE);
+  reco.SetRunVertexFinderTracks(kFALSE);
+  reco.SetRunMultFinder(kFALSE);
+  reco.SetRunTracking(":");
+  reco.SetRunQA(":");
+  reco.SetFillESD(":");
+  //  reco.SetFillESD("PHOS"); 
+  //reco.SetInput("./");
+  reco.SetInput(filename);
+
+  TStopwatch timer;
+  timer.Start();
+  reco.Run();
+  timer.Stop();
+  timer.Print();
+}
